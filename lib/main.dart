f0b3acfb57c8d43f67d2be93d06c657432c3a885
Aleksandra Balon <a.f.w.783@gmail.com>
2024-10-23 00:03:49 +0200

    Add images, google fonts and custom icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather_api/core/injection_container.dart';
 import 'package:flutter_weather_api/features/home/page/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   configureDependencies();
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
+          fontFamily: GoogleFonts.hindSiliguri().fontFamily,
         ),
         home: const HomePage());
   }
